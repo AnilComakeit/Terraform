afb5c97eacd29bc492a2429e7bceedf34ec5b33b
AnilComakeit <90904697+AnilComakeit@users.noreply.github.com>
2022-03-04 10:13:52 +0530

    Update main.tf

--- p2_vpc/main.tf
+++ p2_vpc/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIA5JLBZPYPV44ZR4HQ"
-    secret_key = "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"
+    access_key = var.access_key
+    secret_key = var.secret_key
   
 } 
 
